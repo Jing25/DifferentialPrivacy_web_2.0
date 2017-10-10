aae0fe551dc5d3941b7c4f66bf2f2837d41b40d2
Jing25 <darkljdyx@126.com>
2017-10-10 00:45:14 -0700

    add animation

--- scripts/function_barChart_d3.js
+++ scripts/function_barChart_d3.js
@@ -99,13 +99,6 @@ function redraw() {
 
   }
 
-  function stopAnim() {
-
-
-    //drawBar();
-
-  }
-
  function drawBar() {
 
        y_bar.domain([0, d3.max(dataAnswer, function(d) {return d.number;})+2]);
@@ -146,25 +139,25 @@ function redraw() {
                svg_left.selectAll(".tick:not(:first-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "2,2");
                svg_left.selectAll(".tick text").attr("x", 4).attr("dy", -4);
              }
+             var elem = document.getElementById("btnAnimation");
+             console.log("here", elem.text)
        }
 
    function COAnimation() {
-     var elem = document.getElementById("staAnimation");
-     console.log(elem.value)
-     if (elem.value == "Start Animation") {
-       repeat();
+     var elem = document.getElementById("btnAnimation");
+     console.log(elem.text)
+     if (elem.value == "Answer = ") {
+
        elem.value = "Stop Animation";
      }
      else {
-       stopAnim();
+
        elem.value = "Start Animation";
      }
    }
 
-  var button = d3.select("#staAnimation")
-               .on("click", COAnimation);
-
-
+  // var button = d3.select("#staAnimation")
+  //              .on("click", COAnimation);
 
 }
 
--- scripts/webpage_interaction_jQuery.js
+++ scripts/webpage_interaction_jQuery.js
@@ -1,8 +1,55 @@
-$(document).ready(function() {
+
+
+  $(document).ready(function() {
   $("#btn11").click(function() {
     $(".div2").slideDown(300);
   });
   $("#btn11").click(function() {
     $("#btn11").slideUp(300);
   })
+})
+
+$("#btnAnimation").click(function(){
+  var query = $("#btnQuery");
+  var hl = $("#H_line");
+  var hiv1 = $(".HIV1");
+  var hiv2 = $(".HIV2");
+  var hiv3 = $(".HIV3");
+  var answer = $("#answer");
+
+  function loop() {
+    //query.animate({query.addClass("w3-green")});
+    query.addClass("w3-green");
+    setTimeout(function () {
+      query.removeClass('w3-green');
+    }, 150);
+    hl.animate({width: "100%"}, "slow");
+    // .delay(500).queue(function() {
+    //   hl.removeClass('w3-green').addClass("w3-grey").style({width: "0%"});
+    //   hl.animate({width: "0%"});
+    //});
+     setTimeout(function () {
+       hl.removeClass('w3-green').addClass("w3-grey").css({"width": "0%"})
+     }, 800);
+
+     hl.removeClass("w3-grey").addClass("w3-green");
+     console.log("here")
+
+     setTimeout(function () {
+       hiv1.animate({'backgroundColor': 'rgba(42, 184, 30, 0.64)'}).queue(function(){
+         hiv2.animate({'backgroundColor': 'rgba(42, 184, 30, 0.64)'}).queue(function() {
+         hiv3.animate({'backgroundColor': 'rgba(42, 184, 30, 0.64)'}).dequeue();
+       })
+     })
+      //  hiv3.animate({'backgroundColor': 'rgba(42, 184, 30, 0.64)'}).queue(function(){
+      //       answer.attr("value", "Answer = 3").dequeue();
+      //  })
+     }, 800);
+
+
+     hiv1.css("background-color", "");
+     hiv2.removeAttr("style");
+     hiv3.removeAttr("style");
+  }
+  loop();
 })
